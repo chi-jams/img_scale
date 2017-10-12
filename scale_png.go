3d4ec2f76a4a911e774b182421f77db0ca28b6af
daichij <djameson@mines.edu>
2017-10-12 10:41:26 -0600

    Got rid of a bunch of debug code and uncommented the nocompression flag

--- scale_png.go
+++ scale_png.go
@@ -21,25 +21,17 @@ func main() {
         os.Exit(1)
     }
 
-    fmt.Println("Still here baybee!")
-    fmt.Println(os.Args)
-
     dat, err := ioutil.ReadFile(os.Args[1])
     check(err)
 
     img, err := png.Decode(bytes.NewReader(dat))
-    fmt.Println(img.Bounds().Max.X)
-    fmt.Println(img.Bounds().Max.Y)
 
     file, err := os.Create(os.Args[2])
     defer file.Close()
     check(err)
 
     img_encoder := new(png.Encoder)
-    //img_encoder.CompressionLevel = png.NoCompression
+    img_encoder.CompressionLevel = png.NoCompression
     err = img_encoder.Encode(file, img)
     check(err)
-
-    //err = ioutil.WriteFile(os.Args[2], dat, 0644)
-    //check(err)
 }
