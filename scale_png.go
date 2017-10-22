58a53329a9c162367a58551d05d9056338a0dcf2
daichij <djameson@mines.edu>
2017-10-22 02:13:53 -0600

    Got copying to work through individual pixel manipulation

--- scale_png.go
+++ scale_png.go
@@ -6,8 +6,8 @@ import (
     "fmt"
     "io/ioutil"
     "bytes"
-    "reflect"
     "image"
+    "image/color"
     "image/png"
     "image/jpeg"
 )
@@ -30,15 +30,23 @@ func main() {
     img, err := jpeg.Decode(bytes.NewReader(dat))
 
     bounds := img.Bounds()
-    //pixelWidth := 100
+//    pixelWidth := 100
 
-    fmt.Println(img.At(0,0))
-    fmt.Println(reflect.TypeOf(image.YCbCrToRGB(img.At(0,0))))
     outImg := image.NewRGBA(image.Rect(bounds.Min.X, bounds.Min.Y, bounds.Max.X, bounds.Max.Y))
-    /*
+    for pixelX := bounds.Min.X; pixelX < bounds.Max.X; pixelX++ {
+        for pixelY := bounds.Min.Y; pixelY < bounds.Max.Y; pixelY++ {
+            pr, pg, pb, pa := img.At(pixelX,pixelY).RGBA()
+            var pixColor color.RGBA
+            pixColor.R = uint8(pr>>8)
+            pixColor.G = uint8(pg>>8)
+            pixColor.B = uint8(pb>>8)
+            pixColor.A = uint8(pa>>8)
+            outImg.Set(pixelX, pixelY, pixColor)
+        }
+    }
+/*
     for pixelX := bounds.Min.X; pixelX < bounds.Max.X; i+= pixelWidth {
         for pixelY := bounds.Min.Y; pixelY < bounds.Max.Y; j+= pixelWidth {
-
             pixelValue := 0
             for x := pixelX; x - pixelX < pixelWidth; x++ {
                 for y := pixelY; y - pixelY < pixelWidth; y++ {
@@ -46,16 +54,17 @@ func main() {
                 }
             }
             pixelValue /= pixelWidth ** 2
-
             for x := pixelX; x - pixelX < pixelWidth; x++ {
                 for y := pixelY; y - pixelY < pixelWidth; y++ {
-                    outImg.Set(x, y, pixelValue)
+                    //outImg.Set(x, y, pixelValue)
+                    pr, pg, pb, pa := img.At(x,y).RGBA()
+                    pr := uint8(pr >> 8)
                     outImg.Set(x, y, img.At(i/pixelWidth * pixelWidth, j/pixelWidth * pixelWidth))
                 }
             }
         }
     }
-    */
+*/
 
     file, err := os.Create(os.Args[2])
     defer file.Close()
