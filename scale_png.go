bba9fc90ed13b20a872f82b0d746dd729f1d3d1c
daichij <djameson@mines.edu>
2017-10-23 20:26:14 -0600

    Got an initial pixelating program working

--- scale_png.go
+++ scale_png.go
@@ -18,6 +18,12 @@ func check(e error) {
     }
 }
 
+/*
+func blurSegment(seg image.Image) image.Image {
+    return seg
+}
+*/
+
 func main() {
     if len(os.Args) != 3 {
         fmt.Printf("Usage %v <in_img> <out_img>\n", os.Args[0])
@@ -31,41 +37,42 @@ func main() {
     check(err)
 
     bounds := img.Bounds()
-    //pixelWidth := 100
+    pixelWidth := 25
+
     outImg := image.NewRGBA(image.Rect(bounds.Min.X, bounds.Min.Y, bounds.Max.X, bounds.Max.Y))
-    for pixelX := bounds.Min.X; pixelX < bounds.Max.X; pixelX++ {
-        for pixelY := bounds.Min.Y; pixelY < bounds.Max.Y; pixelY++ {
-            pr, pg, pb, pa := img.At(pixelX,pixelY).RGBA()
-            var pixColor color.RGBA
-            pixColor.R = uint8(pr>>8)
-            pixColor.G = uint8(pg>>8)
-            pixColor.B = uint8(pb>>8)
-            pixColor.A = uint8(pa>>8)
-            outImg.Set(pixelX, pixelY, pixColor)
-        }
-    }
+    for pixelX := bounds.Min.X; pixelX < bounds.Max.X; pixelX+= pixelWidth {
+        for pixelY := bounds.Min.Y; pixelY < bounds.Max.Y; pixelY+= pixelWidth {
 
 /*
-    for pixelX := bounds.Min.X; pixelX < bounds.Max.X; i+= pixelWidth {
-        for pixelY := bounds.Min.Y; pixelY < bounds.Max.Y; j+= pixelWidth {
-            pixelValue := 0
+            blurSegment(img.SubImage(image.Rectangle(pixelX, pixelY,
+                                                     pixelX + pixelWidth,
+                                                     pixelY + pixelWidth)))
+*/
+
+            r, g, b := 0, 0, 0
             for x := pixelX; x - pixelX < pixelWidth; x++ {
                 for y := pixelY; y - pixelY < pixelWidth; y++ {
-                    pixelValue += img.At(x, y)
+                    pr, pg, pb, _ := img.At(x,y).RGBA()
+                    r += int(pr)
+                    g += int(pg)
+                    b += int(pb)
+                    outImg.Set(x, y, img.At(x, y))
                 }
             }
-            pixelValue /= pixelWidth ** 2
+
+            var pixColor color.RGBA
+            pixColor.R = uint8((r / (pixelWidth * pixelWidth )) >> 8)
+            pixColor.G = uint8((g / (pixelWidth * pixelWidth )) >> 8)
+            pixColor.B = uint8((b / (pixelWidth * pixelWidth )) >> 8)
+            pixColor.A = 255
+
             for x := pixelX; x - pixelX < pixelWidth; x++ {
                 for y := pixelY; y - pixelY < pixelWidth; y++ {
-                    //outImg.Set(x, y, pixelValue)
-                    pr, pg, pb, pa := img.At(x,y).RGBA()
-                    pr := uint8(pr >> 8)
-                    outImg.Set(x, y, img.At(i/pixelWidth * pixelWidth, j/pixelWidth * pixelWidth))
+                    outImg.Set(x, y, pixColor)
                 }
             }
         }
     }
-*/
 
     file, err := os.Create(os.Args[2])
     defer file.Close()
