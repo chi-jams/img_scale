c53d9ec83cb92075e4f2e2270467418061c71caa
daichij <djameson@mines.edu>
2018-08-13 16:56:17 -0600

    Discovered that image library has a nice method of reading images already instead of having to do anything silly

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,11 +1,10 @@
 
 extern crate image;
 
-use std::fs::File;
-use std::io::prelude::*;
 use std::{env, process};
+use image::GenericImage;
 
-fn main() -> std::io::Result<()> {
+fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() != 3 {
@@ -13,17 +12,16 @@ fn main() -> std::io::Result<()> {
         process::exit(-1);
     }
 
-    println!("In file: {}", args[1]);
-    println!("Out file: {}", args[2]);
+    let img = image::open(&args[1]).unwrap();
 
-    let mut file = File::open(&args[1])?;
+    println!("Image is {:?}", img.dimensions());
 
+    /*
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
     println!("{}", contents);
 
     let mut out_file = File::create(&args[2])?;
     out_file.write_all(&contents.into_bytes())?;
-
-    Ok(())
+    */
 }
