18732e3240f351c11a495da541731de162b1100e
daichij <djameson@mines.edu>
2018-08-13 16:40:47 -0600

    Got basic file io working

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,9 +1,11 @@
 
 extern crate image;
 
+use std::fs::File;
+use std::io::prelude::*;
 use std::{env, process};
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
 
     if args.len() != 3 {
@@ -13,4 +15,15 @@ fn main() {
 
     println!("In file: {}", args[1]);
     println!("Out file: {}", args[2]);
+
+    let mut file = File::open(&args[1])?;
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    println!("{}", contents);
+
+    let mut out_file = File::create(&args[2])?;
+    out_file.write_all(&contents.into_bytes())?;
+
+    Ok(())
 }
